8324d309d1e693ee1473472b773e316d7bbc8853
Khushal <khushal.hirani4@gmail.com>
2025-03-27 01:14:42 +0530

    try except added

--- Day-4/csv_to_json.py
+++ Day-4/csv_to_json.py
@@ -1,8 +1,12 @@
 import csv
 import json
 
-with open('students.csv', 'r') as new_file:
-        csv_reader = csv.DictReader(new_file)    
-        data = list(csv_reader)
-        json_data = json.dumps(data, indent=2)
-        print(json_data)
+try:
+        with open('students.csv', 'r') as new_file:
+                csv_reader = csv.DictReader(new_file)    
+                data = list(csv_reader)
+                json_data = json.dumps(data, indent=2)
+                print(json_data)
+
+except:
+        print("This file doesn't exist.")
